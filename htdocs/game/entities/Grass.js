ed0657b0a56988803b497ef6fa7c6b3d5c3d4c8e
Jiří Sikora <jirisikoracz@email.cz>
2020-11-18 08:16:03 +0100

    grass

---
+++ htdocs/game/entities/Grass.js
@@ -0,0 +1,15 @@
+
+
+class Grass extends GameObject {
+    constructor(x, y){
+        super(x, y, 16, 16);
+
+        this.layer = y -999;
+
+        this.img = img_grass;
+        this.imgIndex = round(random(1,10));
+    }
+    draw(){
+        image(this.img, this.pos.x, this.pos.y, this.w, this.h, this.imgIndex * this.w, 0, this.w, this.h);
+    }
+}

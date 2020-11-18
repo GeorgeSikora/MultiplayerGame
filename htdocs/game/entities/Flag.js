98feed153b386969cf59b3854b443bf20026f0fb
Jiří Sikora <jirisikoracz@email.cz>
2020-11-18 08:17:04 +0100

    flag for minigames

--- htdocs/game/entities/Flag.js
+++ htdocs/game/entities/Flag.js
@@ -1,12 +1,30 @@
 
 class Flag extends GameObject {
-    constructor(x, y){
-        super(x, y, img_flag.width, img_flag.height);
+    constructor(x, y, team){
+        super(x, y, 96, 128);
+
+        this.center = {x: 14, y: 122};
+
+        
+        this.layer = this.h - this.center.y;
+
+        if(team === 'red'){
+            this.img = img_flag_red;
+        } else {
+            this.img = img_flag_blue;
+        }
+
+        objects.push(new TextSign('Press [E] to capture flag', x, y - this.h - 12));
+        this.textSign = objects[objects.length-1];
+        
     }
     draw(){
         push();
-        translate(0,0,0);
-        image(img_flag, 0, 0);
+        translate(this.pos.x - this.center.x, this.pos.y - this.center.y);
+        let frame = floor(frameCount / 13) % 3;
+        image(this.img, 0, 0, this.w, this.h, frame * this.w, 0, this.w, this.h);
         pop();
+
+        this.textSign.show = (dist(player.pos.x, player.pos.y, this.pos.x, this.pos.y) < 100);
     }
 }

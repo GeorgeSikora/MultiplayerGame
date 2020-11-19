4410b68a9fcfa01803b20bf38a9bf05f6e801f70
GeorgeSikora <jirisikoracz@email.cz>
2020-11-19 19:43:09 +0100

    new object

---
+++ htdocs/game/entities/ScreenFlash.js
@@ -0,0 +1,21 @@
+
+let splash;
+
+class ScreenFlash {
+    constructor(){
+        this.opacity = 255;
+        this.targetOpacity = 0;
+    }
+    draw(){
+        if(this.opacity <= 1) return;
+
+        push();
+        rectMode(CORNER);
+        fill(0,this.opacity);
+        noStroke();
+        rect(0,0,width,height);
+        pop();
+
+        this.opacity += (this.targetOpacity - this.opacity) * 0.04;
+    }
+}

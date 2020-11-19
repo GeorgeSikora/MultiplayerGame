8050f77d57fed19e361223b97cb6ce062f2959dd
Jiří Sikora <jirisikoracz@email.cz>
2020-11-19 02:45:46 +0100

    flag update

--- htdocs/game/entities/Flag.js
+++ htdocs/game/entities/Flag.js
@@ -8,23 +8,93 @@ class Flag extends GameObject {
         this.layer = this.pos.y + this.h - this.center.y;
         this.team = team;
 
-        if(team === 'red'){
-            this.img = img_flag_red;
-        } else {
-            this.img = img_flag_blue;
-        }
+        this.captureReady = false;
+
+        this.captured = false;
 
         objects.push(new TextSign('Press [E] to capture flag', x, y - this.h - 12));
         this.textSign = objects[objects.length-1];
     }
     draw(){
+        if(dist(player.pos.x, player.pos.y, this.pos.x, this.pos.y) < 100){
+            if(this.team == player.team){
+
+                // flag of MY team
+
+                if(player.capturedFlag != null){
+
+                    // player HAVE flag
+
+                    this.acceptReady = true;
+                    this.textSign.str = 'Press [E] to accept flag !';
+                }
+            } else {
+
+                // flag of ENEMY team
+
+                if(!this.captured && player.capturedFlag == null) {
+
+                    // player can capture the flag
+
+                    this.captureReady = true;
+                    this.textSign.str = 'Press [E] to capture flag';
+                }
+            }
+        } else {
+            this.acceptReady = false;
+            this.captureReady = false;
+        }
+        this.textSign.show = this.captureReady | this.acceptReady;
+
         push();
         translate(this.pos.x - this.center.x, this.pos.y - this.center.y);
-        let frame = floor(frameCount / 13) % 3;
-        image(this.img, 0, 0, this.w, this.h, frame * this.w, 0, this.w, this.h);
+        image(img_flag_stick, 0, 0);
+        if(!this.captured){
+            tint(this.team);
+            const frame = floor(frameCount / 13) % 3;
+            image(img_flag_banner, 0, 0, img_flag_banner.width/3, img_flag_banner.height, frame * img_flag_banner.width/3, 0, img_flag_banner.width/3, img_flag_banner.height);
+        }
         pop();
 
-        this.textSign.show = (dist(player.pos.x, player.pos.y, this.pos.x, this.pos.y) < 100);
+    }
+    cap(state){
+        this.captured = state;
+        return this;
+    }
+    keyPressed(){
+
+        if(key == 'e'){
+            
+            // key E pressed
+
+            if(this.captureReady && player.capturedFlag == null){
+                
+                // key E pressed
+
+                sound_bye.play();
+                this.captured = true;
+                player.capturedFlag = this.team;
+                console.log('Captured ' + this.team + ' flag!');
+                this.captureReady = false;
+            }
+
+            if(this.acceptReady) {
+                
+                // key E pressed
+
+                for(var i = 0; i < objects.length; i++){
+                    if(objects[i].constructor.name != 'Flag') continue;
+                    if(objects[i].team == player.capturedFlag) {
+                        objects[i].captured = false;
+                        sound_yay.play();
+                    }
+                }
+                player.capturedFlag = null;
+                this.acceptReady = false;
+            }
+
+
+        }
     }
     remove(){
         removeObject(this.textSign);

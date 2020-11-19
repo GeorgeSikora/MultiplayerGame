fe2680cbb6203073ab8fc95c48be1a6e5ab1a4e7
Jiří Sikora <jirisikoracz@email.cz>
2020-11-19 02:46:50 +0100

    smooth opacity transition

--- htdocs/game/entities/TextSign.js
+++ htdocs/game/entities/TextSign.js
@@ -4,17 +4,23 @@ class TextSign extends GameObject {
     constructor(str, x, y){
         super(x, y, 0, 0);
         this.str = str;
-        this.show = true;
-        this.layer = 100;
+        // CHANGE THIS TO "TRUE" !!! for later..
+        this.show = false;
+        this.opacity = 0;
+        this.targetOpacity = 0;
+        // and also opacity to 255
+        this.layer = y + 1000;
     }
     draw(){
-        if(!this.show) return;
+        this.targetOpacity = this.show ? 255 : 0;
+
+        this.opacity += (this.targetOpacity - this.opacity) * 0.2;
 
         push();
-        fill(0, 127);
+        fill(0, this.opacity/2);
         rectMode(CENTER);
         rect(this.pos.x, this.pos.y +3, 240, 30);
-        fill(255);
+        fill(255, this.opacity);
         textSize(20);
         textAlign(CENTER, CENTER);
         text(this.str, this.pos.x, this.pos.y);

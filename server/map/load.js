932c6c6aa4c6fced509702e42ada4708091171c4
GeorgeSikora <jirisikoracz@email.cz>
2020-11-20 19:32:01 +0100

    map load moved to function

--- server/map/load.js
+++ server/map/load.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
 
-try {  
-    const data = fs.readFileSync('map/map_flags.txt', 'utf8');
-    const lines = data.split(/\r?\n/);
+function loadMap(){
 
-    for(var i = 0; i < lines.length; i++){
-        const param = lines[i].split(" ");
-        const x = parseInt(param[0]);
-        const y = parseInt(param[1]);
-        objects.push(new Block(x,y));
+    try {  
+        const data = fs.readFileSync('map/map_flags.txt', 'utf8');
+        const lines = data.split(/\r?\n/);
+
+        console.log('loading ' + lines.length + ' lines');
+
+        for(var i = 0; i < lines.length; i++){
+            const param = lines[i].split(" ");
+            const x = parseInt(param[0]);
+            const y = parseInt(param[1]);
+            objects.push(new Block(x,y));
+        }
+
+    } catch(e) {
+        console.log('Error:', e.stack);
     }
 
-} catch(e) {
-    console.log('Error:', e.stack);
+    objects.push(new Flag(-1400, 0, 'red'));
+    objects.push(new Flag(1400, 0, 'blue'));
 }
 
-
-objects.push(new Flag(-1400, 0, 'red'));
-objects.push(new Flag(1400, 0, 'blue'));
+module.exports = loadMap;

8c75a2dc339e8a525c42a4191e58cb6b7a7c71fe
GeorgeSikora <jirisikoracz@email.cz>
2020-11-20 19:27:37 +0100

    reset tint to default

--- htdocs/game/entities/Block.js
+++ htdocs/game/entities/Block.js
@@ -12,6 +12,7 @@ class Block extends GameObject {
     }
     draw(){
         this.layer = this.pos.y - player.h - this.h;
+        tint(255);
         image(img_block, this.pos.x -this.w/2, this.pos.y -this.h/2);
     }
 }

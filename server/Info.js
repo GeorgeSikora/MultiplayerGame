30828aa4b152e8786a139f941033a884906bca4f
GeorgeSikora <jirisikoracz@email.cz>
2020-11-20 08:42:24 +0100

    I think its not necessary to make run more servers

---
+++ server/Info.js
@@ -0,0 +1,20 @@
+const ioInfo = io.of('/info');
+
+// middleware
+ioInfo.use((socket, next) => {
+    const query = socket.handshake.query;
+    const user = query.user;
+    const pass = query.pass;
+
+    /* TODO: Ověří přihlašovací údaje z MySQL databáze */
+    return next();
+
+    if (user === 'admin' && pass === 's8j2m6x4n1') {
+        return next();
+    }
+    return next(new Error('Authentication error'));
+});
+
+ioInfo.on('connection', socket => {
+    console.log('New client connected');
+});

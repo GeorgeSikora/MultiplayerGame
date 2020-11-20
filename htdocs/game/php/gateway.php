4ff4e54c00b11e24d0fc317e4006d3314e8833b3
Jiří Sikora <jirisikoracz@email.cz>
2020-11-20 07:42:54 +0100

    setting also port

--- htdocs/game/php/gateway.php
+++ htdocs/game/php/gateway.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 
 <?php
-    if(isset($_POST['name']) && isset($_POST['color'])) { ?>
+    if(isset($_GET['name']) && isset($_GET['color'])) { ?>
 
     <script>
         const post = {
-            name:    '<?php echo $_POST['name'];?>',
-            color:   '<?php echo $_POST['color'];?>'
+            name:    '<?php echo $_GET['name'];?>',
+            color:   '<?php echo $_GET['color'];?>'
         };
+        const PORT = '<?php if(isset($_GET['port'])) echo $_GET['port']; else echo 1771;?>'; 
+        console.log('PORT = ' + PORT);
     </script>
 
     <?php } else {

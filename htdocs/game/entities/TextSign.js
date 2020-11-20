51d4497bfe741763dd02254d82680a1f56624992
GeorgeSikora <jirisikoracz@email.cz>
2020-11-20 19:29:48 +0100

    custom color

--- htdocs/game/entities/TextSign.js
+++ htdocs/game/entities/TextSign.js
@@ -4,6 +4,7 @@ class TextSign extends GameObject {
     constructor(str, x, y){
         super(x, y, 0, 0);
         this.str = str;
+        this.color = 'white';
         // CHANGE THIS TO "TRUE" !!! for later..
         this.show = false;
         this.opacity = 0;
@@ -20,7 +21,9 @@ class TextSign extends GameObject {
         fill(0, this.opacity/2);
         rectMode(CENTER);
         rect(this.pos.x, this.pos.y +3, 240, 30);
-        fill(255, this.opacity);
+        var col = color(this.color);
+        col.setAlpha(this.opacity);
+        fill(col);
         textSize(20);
         textAlign(CENTER, CENTER);
         text(this.str, this.pos.x, this.pos.y);

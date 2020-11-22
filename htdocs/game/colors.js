631664f3268a2b3d1214b5de2b4f97bc00a0b9fe
Jiří Sikora <jirisikoracz@email.cz>
2020-11-22 07:45:40 +0100

    cleanup

--- htdocs/game/colors.js
+++ htdocs/game/colors.js
@@ -1,45 +1,20 @@
-const BLACK   = '#000000';
-const WHITE   = '#FFFFFF';
 
-const RED     = '#DF0000';
-const ORANGE  = '#FF8000';
-const YELLOW  = '#FFFF00';
-const GREEN   = '#00FF00';
-const AQUA    = '#00D4D4';
-const BLUE    = '#5078FF';
-const PURPLE  = '#50007F';
-
-const GRAY   = '#666666';
-const PINK   = '#FF69B4';
-
-function getColor(id){
-    switch(id){
-        case '0': return RED;
-        case '1': return ORANGE;
-        case '2': return YELLOW;
-        case '3': return GREEN;
-        case '4': return AQUA;
-        case '5': return BLUE;
-        case '6': return PURPLE;
-    }
-}
-
-/* OLD
-function getColorToken(id){
-    switch(id){
-        case '0': return '2';
-        case '1': return '3';
-        case '2': return '4';
-        case '3': return '5';
-        case '4': return '6';
-        case '5': return '7';
-        case '6': return '8';
-    }
-}
-*/
+const BLACK   = '#000000'; // 0
+const WHITE   = '#FFFFFF'; // 1
+const RED     = '#DF0000'; // 2
+const ORANGE  = '#FF8000'; // 3
+const YELLOW  = '#FFFF00'; // 4
+const GREEN   = '#00FF00'; // 5
+const AQUA    = '#00D4D4'; // 6
+const BLUE    = '#5078FF'; // 7
+const PURPLE  = '#50007F'; // 8
+const GRAY    = '#666666'; // 9
+const PINK    = '#FF69B4'; // A
 
 function getColorToken(id){
     switch(id){
+        case 'black':   return '0';
+        case 'white':   return '1';
         case 'red':     return '2';
         case 'orange':  return '3';
         case 'yellow':  return '4';
@@ -47,6 +22,8 @@ function getColorToken(id){
         case 'aqua':    return '6';
         case 'blue':    return '7';
         case 'purple':  return '8';
-        default: return '4';
+        case 'gray':    return '9';
+        case 'pink':    return 'a';
+        default: return '4'; // default is yellow
     }
 }

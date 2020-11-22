ab17a1dc5f0158e0d0e49f58aa1e37078e91b752
Jiří Sikora <jirisikoracz@email.cz>
2020-11-22 07:48:36 +0100

    moved to the main.js instead of game.js

--- htdocs/game/game.js
+++ htdocs/game/game.js
@@ -1,225 +1,102 @@
 
-/* HERE PUT YOUR SERVER IP OR URL WITH PORT */
-let SERVER_URL = '185.221.124.205:3031/client'; 
-
-/*** MAIN SETUP ***/
-function setup() {
-  /* create as monitor screen size */
-  createCanvas(window.screen.width, window.screen.height, WEBGL);
-  /* resize to the browser window */
-  windowResized();
-  /* set some default stuff */
-  textFont(font_default);
-  noStroke();
-  /* create necessary game objects */
-  player = new MyPlayer(0, post.name, 0, 0, 'lobby');
-  player.enable = false;
-  player.show = false;
-
-  cam = new Camera(player);
-  chat = new Chat();
-  splash = new ScreenFlash();
-  minimap = new Minimap();
-
-  /* connect to the multiplayer server */
-  multiplayer = new Multiplayer(SERVER_URL);
-  socket.emit('get-player-teams');
-
-  sound_drop2.play();
-  gameRestart();
-
-  /*
-  objects.push(new Flag(-200, -200, 'red'));
-  objects.push(new Flag( 200, -200, 'green'));
-  objects.push(new Flag(-200, 200, 'blue').cap(true));
-  objects.push(new Flag( 200, 200, 'yellow'));
-  objects.push(new DroppedFlag( 100, 100, 'blue'));
-  */
- /*
- objects.push(new FlagSelect(-200, 0, 'red'));
- objects.push(new FlagSelect( 200, 0, 'blue'));
-
-  sound_drop2.play();
-  music_menu_id = music_menu.play();
-  music_menu.volume(musicVolume, music_menu_id);
-  */
-}
-
+/* GLOBAL VARIABLES */
 let inGame = false;
-
-let menuOpened = false;
 let muted = false;
 let buildingEnable = false;
 let finalDrawTime = 0;
+let gameLoaded = false;
+let gameStarted = false;
+let fps = 0;
+let music_end_id;
+const SEND_GAP = 90; // player pos sender
+let serverConst = []; // server constants
+let menuOpened = false;
 
-let music, musicID;
+const game = {
 
-/*** MAIN LOOP ***/
-function draw() {
+    teams: {
+        red: 0,
+        blue: 0
+    },
 
-  /* FPS drop check */
-  if(frameRate() < 15){
-    //chat.add("fps drop " + frameRate() + " fps");
-    return;
-  }
+    setup(){
 
-  var drawTime = millis();
+        player  = new MyPlayer(0, post.name, 0, 0, 'lobby');
+        cam     = new Camera(player);
+        chat    = new Chat();
+        splash  = new ScreenFlash();
+        minimap = new Minimap();
 
-  /* calc player move */
-  player.refresh();
+        player.enable = false;
+        player.show = false;
 
-  /* send vars and etc. to server */
-  multiplayer.refresh();
+        /* connect to the multiplayer server */
+        multiplayer = new Multiplayer(SERVER_URL);
+        socket.emit('get-player-teams');
 
-  /* chceck and repair positions of coliding objects */
-  checkCollisions();
-  /*
-  for(var i = 0; i < objects.length; i++){
-    if(objects[i].constructor.name === 'Block'){
-      player.collision.collideRect(objects[i]);
+        game.restart();
+        sound_drop2.play();
+    },
+    start(){
+        //Howler.stop();
+      
+        console.log('game started');
+        minimap.enable = true;
+        inGame = true;
+        //music_menu.rate(3, music_menu_id);
+        music_menu.fade(volumeMusic, 0, 2000, music_menu_id);
+    },
+    end() {
+        music_end_id = music_end.play();
+        music_end.volume(volumeMusic, music_end_id);
+        music_end.fade(0, volumeMusic, 1000, music_end_id);
+      
+        player.enable = false;
+      
+        cam.target = null;
+        cam.easing = 0.02;
+        cam.targetPos = {x: 0, y: 0};
+        cam.targetScale = 0.35;
+      
+        minimap.enable = false;
+    },
+    restart() {
+
+        music_end.fade(volumeMusic, 0.0, 3000, music_end_id);
+
+        setTimeout(()=>{
+          if(inGame) return;
+          Howler.stop();
+          music_menu_id = music_menu.play();
+          music_menu.volume(volumeMusic, music_menu_id);
+        }, 3000);
+      
+        gameStarted = false;
+      
+        socket.disconnect();
+      
+        objects = [];
+        players = [];
+        
+        player = new MyPlayer(0, post.name, 0, 0, 'white');
+        player.enable = false;
+        player.show = false;
+      
+        cam = new Camera(player);
+      
+        /* connect to the multiplayer server */
+        multiplayer = new Multiplayer(SERVER_URL);
+        /* set default volume at 30% */
+        Howler.volume(0.3);
+      
+        objects.push(new FlagSelect(-200, 50, 'red'));
+        objects.push(new FlagSelect( 200, 50, 'blue'));
+        
+        splash = new ScreenFlash();
+        
+        socket.emit('get-player-teams');
+      
+        minimap.enable = false;
+        inGame = false;
     }
-  }
-  */
-
-  /* cam ortho */
-  cam.ortho();
-
-  /* background color */
-  //background(100, 155, 74);
-  background(12);
-
-  /* draw Safe Zone */
-  drawSavezone();
-
-  /* update and draw objects */
-  var objectsRender = [];
-  for (var i = 0; i < objects.length; i++) {
-    var obj = objects[i];
-    obj.update();
-    if(objects[i] == null) continue;
-    /* draw only when it is on screen */
-    if(rectRect(obj.pos.x-obj.center.x, obj.pos.y-obj.center.y, obj.w, obj.h, cam.pos.x-width/cam.scale/2, cam.pos.y-height/cam.scale/2, width/cam.scale, height/cam.scale)){
-      //obj.draw();
-      objectsRender.push(obj);
-    }
-  }
-
-  objectsRender.push(player);
-  for (var i = 0; i < players.length; i++) {
-    objectsRender.push(players[i]);
-    //players[i].draw();
-  }
-
-  objectsRender.sort(function(a, b) {
-      return a.layer - b.layer;
-  });
-
-  for (var i = 0; i < objectsRender.length; i++) {
-    objectsRender[i].draw();
-  }
-  
-  //console.log(objectsRender);
-
-  /* refresh and draw other players on server */
-  for (var i = 0; i < players.length; i++) {
-    players[i].refresh();
-    //players[i].draw();
-  }
-
-  /* draw my player */
-  //player.draw();
-  
-  /* set left top corner ortho pos x0 y0 */
-  ortho(0, width, -height, 0);
-
-  /* minimap */
-  minimap.draw();
-
-  /* informations corner */
-  drawInfo();
-
-  tint(255);
-  if(muted) {
-    image(ico_sounds_off, width-32, 0);
-  }else{
-    image(ico_sounds_on, width-32, 0);
-  }
-
-  chat.draw(10,height-10);
-
-  splash.draw();
-
-  if(inGame && !socket.connected) {
-    gameRestart();
-  }
-
-  if(frameCount%50 == 0) finalDrawTime = (millis() - drawTime).toFixed(2);
-}
-
-function gameStart(){
-  //Howler.stop();
-
-  console.log('game started');
-  minimap.enable = true;
-  inGame = true;
-  //music_menu.rate(3, music_menu_id);
-  music_menu.fade(volumeMusic, 0, 2000, music_menu_id);
-}
-
-var music_end_id;
-
-function gameEnd(){
-  music_end_id = music_end.play();
-  music_end.volume(musicVolume, music_end_id);
-  music_end.fade(0, volumeMusic, 1000, music_end_id);
-
-  player.enable = false;
-
-  cam.target = null;
-  cam.easing = 0.02;
-  cam.targetPos = {x: 0, y: 0};
-  cam.targetScale = 0.35;
-
-  minimap.enable = false;
-}
-
-function gameRestart(){
-  music_end.fade(volumeMusic, 0.05, 3000, music_end_id);
-  setTimeout(()=>{
-
-    if(inGame) return;
-
-    Howler.stop();
-    music_menu_id = music_menu.play();
-    music_menu.volume(volumeMusic, music_menu_id);
-  }, 3000);
-
-
-  gameStarted = false;
-
-  socket.disconnect();
-
-  objects = [];
-  players = [];
-  
-  player = new MyPlayer(0, post.name, 0, 0, 'white');
-  player.enable = false;
-  player.show = false;
-
-  cam = new Camera(player);
-
-  /* connect to the multiplayer server */
-  multiplayer = new Multiplayer(SERVER_URL);
-  /* set default volume at 30% */
-  Howler.volume(0.3);
-
-  objects.push(new FlagSelect(-200, 50, 'red'));
-  objects.push(new FlagSelect( 200, 50, 'blue'));
-  
-  splash = new ScreenFlash();
-  
-  socket.emit('get-player-teams');
-
-  minimap.enable = false;
-  inGame = false;
-}
+};

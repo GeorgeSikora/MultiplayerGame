e0478c1c3ae82cf1453a75c608bcd0d8788d1b6e
Jiří Sikora <jirisikoracz@email.cz>
2020-11-27 08:17:33 +0100

    block as tile

--- htdocs/game/entities/Block.js
+++ htdocs/game/entities/Block.js
@@ -6,6 +6,13 @@ class Block extends GameObject {
         this.collision = new Collision(this);
         this.collision.static = true;
         this.center = {x: this.w/2, y: this.h/2};
+
+        this.TL = 0;
+        this.TR = 0;
+        this.BL = 0;
+        this.BR = 0;
+
+        this.texture = null;
     }
     update(){
         
@@ -13,6 +20,82 @@ class Block extends GameObject {
     draw(){
         this.layer = this.pos.y - player.h - this.h;
         tint(255);
-        image(img_block, this.pos.x -this.w/2, this.pos.y -this.h/2);
+        //image(tileStructure.island, this.pos.x -this.w/2, this.pos.y -this.h/2);
+
+        /*
+        if(this.texture != null) {
+            image(this.texture, this.pos.x -this.w/2, this.pos.y -this.h/2);
+        }
+        */
+
+        if (this.TL==0 && this.TR==0 && this.BL==0 && this.BR==0) {
+            image(tileStructure.island, this.pos.x-hsize, this.pos.y-hsize);
+        } else {
+            image(tileStructure.TL[this.TL], this.pos.x-hsize, this.pos.y-hsize);
+            image(tileStructure.TR[this.TR], this.pos.x, this.pos.y-hsize);
+            image(tileStructure.BL[this.BL], this.pos.x-hsize, this.pos.y);
+            image(tileStructure.BR[this.BR], this.pos.x, this.pos.y);
+
+        }
+        
     }
+
+    autoTile() {
+        const size = 64;
+
+        this.TL = 0;
+        this.TR = 0;
+        this.BL = 0;
+        this.BR = 0;
+
+        if (getTile(this.pos.x, this.pos.y-size) == 1) {
+            this.TL+=2;
+            this.TR+=1;
+        }
+        if (getTile(this.pos.x, this.pos.y+size) == 1) {
+            this.BL+=1;
+            this.BR+=2;
+        }
+        if (getTile(this.pos.x-size, this.pos.y) == 1) {
+            this.TL+=1;
+            this.BL+=2;
+        }
+        if (getTile(this.pos.x+size, this.pos.y) == 1) {
+            this.TR+=2;
+            this.BR+=1;
+        }
+
+        if (getTile(this.pos.x-size, this.pos.y-size) == 1 && this.TL==3) this.TL=4;
+        if (getTile(this.pos.x+size, this.pos.y-size) == 1 && this.TR==3) this.TR=4;
+        if (getTile(this.pos.x-size, this.pos.y+size) == 1 && this.BL==3) this.BL=4;
+        if (getTile(this.pos.x+size, this.pos.y+size) == 1 && this.BR==3) this.BR=4;
+
+        /*
+        var buff = createGraphics(64, 64);
+
+        if (this.TL==0 && this.TR==0 && this.BL==0 && this.BR==0) {
+            buff.image(tileStructure.island, 0, 0);
+        } else {
+            buff.image(tileStructure.TL[this.TL], 0, 0);
+            buff.image(tileStructure.TR[this.TR], hsize, 0);
+            buff.image(tileStructure.BL[this.BL], 0, hsize);
+            buff.image(tileStructure.BR[this.BR], hsize, hsize);
+        }
+        this.texture = buff;
+        buff.remove();
+        */
+    }
+}
+
+function getTile(x, y) {
+    for(var i = 0; i < objects.length; i++) {
+        if(objects[i].constructor.name == 'Block') {
+            //console.log('yes!');
+            const pos = objects[i].pos;
+            if(pos.x == x && pos.y == y){
+                return 1;
+            }
+        }
+    }
+    return 0;
 }

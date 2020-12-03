69e6dfa0df5c0374713848890eceba9953dc24e4
GeorgeSikora <jirisikoracz@email.cz>
2020-12-03 13:25:13 +0100

    upgrades

--- htdocs/game/entities/TileStructure.js
+++ htdocs/game/entities/TileStructure.js
@@ -21,73 +21,31 @@ function loadTiles() {
 }
 
 class TileStructure {
-    constructor() {
-        //let TL = [], TR = [], BL = [], BR = [], island;
 
-      this.grassStructure = [ 6, 9, 5, 15, 14,
-                              7, 10, 1, 13, 3,
-                              2, 8, 4, 12, 11];
-      this.grass = [16];
+  static idMap = [ 
+    6,  9, 5, 15, 14,
+    7, 10, 1, 13, 3,
+    2,  8, 4, 12, 11
+  ];
 
-      const tileset = tileset_dirt;
-      console.log(tileset.width, tileset.height);
-
-      for(var i = 0; i < 15; i++) {
-        const x = i%(tileset.width/32);
-        const y = floor((i*32)/tileset.width);
-
-        //console.log(x, y);
-
-        this.grass[ this.grassStructure[i]] = tileset.get(x*32, y*32, 32, 32);
-      }
-
-
-        this.TL = [];
-        this.TR = [];
-        this.BL = [];
-        this.BR = [];
-
-        this.size = 64;
-        
-        /*
-        TL = new PImage[5];
-        TR = new PImage[5];
-        BL = new PImage[5];
-        BR = new PImage[5];
-        */
-
-        this.createTile(3, 3, 3, 3, this.size, 0           );
-        this.createTile(0, 2, 1, 4, 0,    this.size        );
-        this.createTile(1, 0, 4, 2, this.size, this.size   );
-        this.createTile(2, 4, 0, 1, 0,    this.size*2      );
-        this.createTile(4, 1, 2, 0, this.size, this.size*2 );
-    
-        this.island = tileset_grass.get(0, 0, this.size, this.size);
-    }
-
-    createTile(TL_ID, TR_ID, BL_ID, BR_ID, x, y) {
-      var t = cutTile(x, y);
-      this.TL[TL_ID] = t[0];
-      this.TR[TR_ID] = t[1];
-      this.BL[BL_ID] = t[2];
-      this.BR[BR_ID] = t[3];
-    }
+  constructor() {
+    this.grass = loadTileset(tileset_dirt);
+  }
 }
 
-function cutTile(x, y) {
-    var TL, TR, BL, BR;
-    TL = tileset_grass.get(x, y, hsize, hsize);
-    TR = tileset_grass.get(x+hsize, y, hsize, hsize);
-    BL = tileset_grass.get(x, y+hsize, hsize, hsize);
-    BR = tileset_grass.get(x+hsize, y+hsize, hsize, hsize);
-    return [TL, TR, BL, BR];
+function loadTileset(img) {
+  var tileset = [16];
+
+  // set default preview
+  tileset[0] = img.get(1 * 32, 1 * 32, 32, 32);
+
+  // load every tile to the array
+  for(var i = 0; i < 15; i++) {
+    // get position from index
+    const x = i%(img.width/32);
+    const y = floor((i*32)/img.width);
+    // set the index to specific tile
+    tileset[TileStructure.idMap[i]] = img.get(x * 32, y * 32, 32, 32);
   }
-/*
-  setInterval(() => {
-    for(var i = 0; i < objects.length; i++) {
-        if(objects[i].constructor.name == 'Block') {
-          objects[i].autoTile();
-        }
-      }
-  }, 30000);
-  */
+  return tileset;
+}

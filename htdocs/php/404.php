7665f71f6040a6d50f35ff4f0daff4a5c0151328
Jiří Sikora <jirisikoracz@email.cz>
2021-01-01 22:21:12 +0100

    ading 404 page

---
+++ htdocs/php/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Capture the flag</title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+<div class="content">
+
+    <img src="style/images/flags-logo.png" width="80%" alt="Banner">
+
+    <h1 class="title">404 Page not found!</h1>
+    
+    <div class="link"><a href="./home" class="link">Home</a></div>
+
+</div>
+
+<div class="bottom-text">
+    <p>Copyright © George Sikora 2020</p>
+</div>
+
+</body>
+</html>

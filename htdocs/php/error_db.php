526eb1403d1c44ad0fcf73e969fc143e842b63c4
Jiří Sikora <jirisikoracz@email.cz>
2021-01-02 23:09:14 +0100

    error handling

---
+++ htdocs/php/error_db.php
@@ -0,0 +1,10 @@
+
+<div class="content">
+
+    <img src="/style/images/flags-logo.png" width="80%" alt="Banner">
+
+    <h1 class="error-title">Cannot connect to Database</h1>
+    <p class="error-description">ERROR#<?php echo $error ?></p>
+    <div class="links"><a href="/home">Home</a></div>
+
+</div>

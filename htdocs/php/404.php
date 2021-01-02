1a58a544ed55c75998af65924c70c24f278664ec
Jiří Sikora <jirisikoracz@email.cz>
2021-01-02 23:09:01 +0100

    404 custom page

--- htdocs/php/404.php
+++ htdocs/php/404.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Capture the flag</title>
-    <link rel="stylesheet" href="style/main.css">
-</head>
-<body>
+
 <div class="content">
 
-    <img src="style/images/flags-logo.png" width="80%" alt="Banner">
+    <img src="/style/images/flags-logo.png" width="80%" alt="Banner">
 
-    <h1 class="title">404 Page not found!</h1>
-    
-    <div class="link"><a href="./home" class="link">Home</a></div>
+    <h1 class="error-title">Error 404</h1>
+    <p class="error-description">Page not found!</p>
+    <div class="links"><a href="/home">Home</a></div>
 
 </div>
-
-<div class="bottom-text">
-    <p>Copyright © George Sikora 2020</p>
-</div>
-
-</body>
-</html>

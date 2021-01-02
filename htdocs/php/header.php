6b531be5db42c95d5353865026fe6b689867d068
Jiří Sikora <jirisikoracz@email.cz>
2021-01-02 23:09:34 +0100

    header of page

---
+++ htdocs/php/header.php
@@ -0,0 +1,18 @@
+
+<!-- title and icon -->
+<title>Capture the flag</title>
+<link rel="icon" type="image/ico" href="/style/favicon.ico"/>
+
+<!-- css styles -->
+<link rel="stylesheet" href="/style/main.css">
+
+<!-- JS libraries -->
+
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+<script src="//code.jquery.com/jquery-1.12.4.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+
+<!-- introduction for console -->
+<script src="/js/introduction.js"></script>
+<script src="/js/validateForm.js"></script>

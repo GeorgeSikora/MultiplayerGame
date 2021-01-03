8121a5c884d93ab1c522aa390ae5436f98a26656
Jiří Sikora <jirisikoracz@email.cz>
2021-01-03 20:27:16 +0100

    registration error handling

---
+++ htdocs/php/error_registration.php
@@ -0,0 +1,10 @@
+
+<div class="content">
+
+<img src="/style/images/flags-logo.png" width="80%" alt="Banner">
+
+<h1 class="error-title">Registration error</h1>
+<p class="error-description">Something went wrong!</p>
+<div class="links"><a href="/home">Home</a></div>
+
+</div>

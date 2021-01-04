1a81e036ac94db585d6c7e64e1ef6e4db9735851
GeorgeSikora <jirisikoracz@email.cz>
2021-01-04 16:08:52 +0100

    cleanup

--- htdocs/php/registerGate.php
+++ htdocs/php/registerGate.php
@@ -1,58 +1,58 @@
 <?php
 
-/* Disable printing errors from php */
-//ini_set('display_errors', 0);
-//error_reporting(E_ERROR | E_WARNING | E_PARSE); 
+// display errors from php to page
+define("enable_display_errors", true);
+// sending errors to db
+define("enable_error_sending", true);
 
-//define("FOO",     "something");
 
-//include('error_db.php');
-
-//header('location: /game');
+if (enable_display_errors) {
+    ini_set('display_errors', 0);
+    error_reporting(E_ERROR | E_WARNING | E_PARSE); 
+}
 
 $nickname = $_POST['nickname'];
 $hashpswrd = $_POST['hashpswrd'];
 $confirm = $_POST['confirm'];
 
+/*
 echo '<pre>'.print_r($_POST,true).'</pre>';
-
 echo 'nickname: '   . isset($nickname)  . '<br>';
 echo 'password: '   . isset($hashpswrd) . '<br>';
 echo 'confirm: '    . isset($confirm)   . '<br>';
+*/
 
 if (!isset($nickname)) sendError('registration', 'post nemá nickname');
 if (!isset($hashpswrd)) sendError('registration', 'post nemá heslo');
 if (!isset($confirm)) sendError('registration', 'post nemá potvrzený souhlas s pravidly');
 if (!preg_match('/^[a-zA-Z0-9_\s]*$/', $nickname)) sendError('registration', 'nickname obsahuje nepovolené znaky');
 
-return;
-
+echo 'Úspěch';
 applyToken($token);
 
 mysqli_close($mysqli);
 
 /* send variables to session for the game */
-
 session_start(); 
-$_SESSION['name'] = 'Hovado';
 
+$_SESSION['name'] = $nickname;
+$_SESSION['password'] = $hashpswrd;
 
-
-
+header('location: /game');
 
 function sendError($type, $desc) {
+    if (!enable_error_sending) return;
     include('db_connect.php');
     $ip = $_SERVER['REMOTE_ADDR'];
     $sql = "INSERT INTO errors (Type, Description, Ip) VALUES ('$type', '$desc', '$ip')";
     $result = $mysqli->query($sql);
     if ($result === TRUE) {
         header('location: /error/'.$type); // ERROR inserted to db
-        die();
     } else {
         header('location: /error/db/9901'); // Cannot insert ERROR to db
-        die();
     }
     mysqli_close($mysqli);
+    die();
 }
 
 function applyToken($token) {

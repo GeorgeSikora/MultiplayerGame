a2480b186ecf57cf8e2a6698bc9afb6f7b309885
Jiří Sikora <jirisikoracz@email.cz>
2021-01-05 12:35:48 +0100

    die() instead return

--- htdocs/php/db_connect.php
+++ htdocs/php/db_connect.php
@@ -5,7 +5,7 @@ $mysqli = mysqli_connect('localhost', 'root', '', 'multiplayergame');
 /* check connection */
 if (mysqli_connect_errno()) {
     header('location: /error/db/' . mysqli_connect_errno());
-    return;
+    die();
 }
 
 ?>

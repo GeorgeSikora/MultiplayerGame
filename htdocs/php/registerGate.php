695c604d89b302761662c19e1047ce110042da95
GeorgeSikora <jirisikoracz@email.cz>
2021-01-06 22:26:16 +0100

    handling if user already exists

--- htdocs/php/registerGate.php
+++ htdocs/php/registerGate.php
@@ -1,5 +1,4 @@
 <?php
-/* SETUP */
 
 // display errors from php to page
 define("enable_display_errors", true);
@@ -30,36 +29,65 @@ if (!isset($nickname)) sendError('registration', 'post nemá nickname');
 if (!isset($hashpswrd)) sendError('registration', 'post nemá heslo');
 if (!isset($confirm)) sendError('registration', 'post nemá potvrzený souhlas s pravidly');
 
-if (!preg_match('/^[a-zA-Z0-9_\s]*$/', $nickname)) sendError('registration', 'nickname obsahuje nepovolené znaky');
+//if (strlen($hashpswrd) < 8) sendError('registration', 'nickname obsahuje nepovolené znaky');
+//if (strlen($hashpswrd) > 25) sendError('registration', 'nickname obsahuje nepovolené znaky');
+//if (strlen($hashpswrd) != 32) sendError('registration', 'heslo nemá určitou délku pro md5 hash'); 
+if (strlen($hashpswrd) != 64 && strlen($hashpswrd) != 32) sendError('registration', 'heslo nemá určitou délku pro sha256 hash');
 
-// apply token
-applyToken($token);
+if (strlen($nickname) < 3) sendError('registration', 'nickname obsahuje nepovolené znaky');
+if (strlen($nickname) > 25) sendError('registration', 'nickname obsahuje nepovolené znaky');
+if (!preg_match('/^[a-zA-Z0-9_\s]*$/', $nickname)) sendError('registration', 'nickname obsahuje nepovolené znaky');
 
 // register player to db
 include('db_connect.php');
+
+$sql = "SELECT name FROM players WHERE name='$nickname'";
+$result = $mysqli -> query($sql);
+$count = mysqli_num_rows($result);
+
+if ($count != 0) {
+    session_start();
+    $_SESSION['err'] = 'Jméno již existuje!';
+    //header('location: /register');
+
+    mysqli_close($mysqli);
+
+    echo 'Name already exists!';
+    die();
+}
+
+// exit db connection
+/*
+mysqli_close($mysqli);
+die();
+*/
+
+// apply token
+applyToken($token);
 
 $sql = "INSERT INTO players (name, password) VALUES ('$nickname', '$hashpswrd')";
 $result = $mysqli->query($sql);
 
 if ($result === TRUE) {
-    header('location: /error/'.$type); // Player sucsesfully registered
+    //echo 'ERROR 1!';
+    //header('location: /error/'.$type); // Player sucsesfully registered
 } else {
-    header('location: /error/db/9902'); // Cannot add player to db
+    echo 'ERROR 2!';
+    //header('location: /error/db/9902'); // Cannot add player to db
 }
-
-mysqli_close($mysqli);
-
 
 // exit db connection
 mysqli_close($mysqli);
 
 // session vars for game
 session_start();
-$_SESSION['name']       = $nickname;
-$_SESSION['password']   = $hashpswrd;
+$_SESSION['name']   = $nickname;
+$_SESSION['hashpswrd']  = $hashpswrd;
 
 // go to the game
-header('location: /game');
+
+//header('location: /game');
+die();
 
 /********************
     OWN FUNCTIONS
@@ -72,9 +100,11 @@ function sendError($type, $desc) {
     $sql = "INSERT INTO errors (type, description, ip) VALUES ('$type', '$desc', '$ip')";
     $result = $mysqli->query($sql);
     if ($result === TRUE) {
-        header('location: /error/'.$type); // ERROR inserted to db
+        echo 'ERROR 3!';
+        //header('location: /error/'.$type); // ERROR inserted to db
     } else {
-        header('location: /error/db/9901'); // Cannot insert ERROR to db
+        echo 'ERROR 4!';
+        //header('location: /error/db/9901'); // Cannot insert ERROR to db
     }
     mysqli_close($mysqli);
     die();

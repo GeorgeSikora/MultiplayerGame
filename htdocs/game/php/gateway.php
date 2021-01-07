d507e61820ea63c7025bebfa6f92b458b73f4c11
GeorgeSikora <jirisikoracz@email.cz>
2021-01-07 14:11:10 +0100

    autologin

--- htdocs/game/php/gateway.php
+++ htdocs/game/php/gateway.php
@@ -3,15 +3,33 @@
 <?php
 
     session_start();
+
     if(isset($_SESSION['name']) && isset($_SESSION['hashpswrd'])) { ?>
 
     <script>
+
         const post = {
             name:       '<?php echo $_SESSION['name'];?>',
             hashpswrd:  '<?php echo $_SESSION['hashpswrd'];?>'
         };
 
         console.log(post);
+
+        request = $.ajax({
+        url: "/php/loginGate",
+        type: "post",
+        data: {
+            nickname: post.name,
+            hashpswrd: post.hashpswrd
+        },
+        success: function(result) {
+            if (result) {
+                window.location.href = "/login";
+            }
+        }
+
+    });
+
     </script>
 
     <?php } else {

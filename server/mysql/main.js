1407b9f00c9948db8b3de40db7d99de85a0c30ea
Jiří Sikora <jirisikoracz@email.cz>
2021-01-11 09:02:14 +0100

    cleanup

--- server/mysql/main.js
+++ server/mysql/main.js
@@ -1,12 +1,5 @@
 
-mysqlCon = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'multiplayergame'
-});
-
-mysqlCon.connect();
+//mysqlCon.connect();
 
 /* EXAMPLE
 connection.query('SELECT * FROM players', function (error, results, fields) {
@@ -19,15 +12,22 @@ connection.query('SELECT * FROM players', function (error, results, fields) {
 });
 */
 
+// UPDATE players SET connected=0
+
+mysqlCon.query('UPDATE players SET connected=0', function (error, results, fields) {
+  if (error) throw error;
+});
+
+/*
 mysqlCon.query('SELECT * FROM players', function (error, results, fields) {
     if (error) throw error;
 
-    console.log(results);
+    //console.log(results);
   
     for(result in results) {
       var p = results[result];
       console.log( result + '. name: ' + p.name + ' password: ' + p.password + ' ip: ' + p.ip);
     } 
 });
- 
-mysqlCon.end();
+ */
+//mysqlCon.end();
